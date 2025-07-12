ad179bae294954c756fb2d21c5b0bac03e5ba0ff
Tanecodes <tanecodes@gmail.com>
2025-07-13 02:32:53 +1000

    just doing last commit before i jam a different project might try learn some canvas api and have a break from coding the game

--- game/start-and-menu/start-screen.js
+++ game/start-and-menu/start-screen.js
@@ -1,5 +1,6 @@
 import { audio } from "../audio.js";
-
+// start screen button to set up main menu
+// i coded the main menu button sfx in here aswell
 export function setupStartScreen() {
   const startScreen = document.getElementById("startScreen");
   const startBtn = document.getElementById("startBtn");
@@ -14,4 +15,19 @@ export function setupStartScreen() {
       mainMenu.classList.remove("hidden");
     }, 1000);
   });
-};
+  
+// main menu button sfx
+  const menuButtons = document.querySelectorAll(".menu-btn")
+
+  menuButtons.forEach(btn => {
+    btn.addEventListener("mouseenter", () => {
+      audio.sfx.btnHover.currentTime = 0;
+      audio.sfx.btnHover.play();
+    });
+
+    btn.addEventListener("click", () => {
+      audio.sfx.btnSelect.currentTime = 0;
+      audio.sfx.btnSelect.play();
+    });
+  });
+}
